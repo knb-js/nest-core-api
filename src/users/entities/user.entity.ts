17b0bbaca62a06c0821854f714e8e6490cd7605f
Kahlil Nuñez Bayuk <kah.nunez@duocuc.cl>
2025-02-05 12:03:11 -0300

    test

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,36 @@
-export class User {}
+export class User {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+  
+    constructor(id: number, name: string, email: string, password: string) {
+      this.id = id;
+      this.name = name;
+      this.email = email;
+      this.password = password;
+      this.createdAt = new Date();
+      this.updatedAt = new Date();
+    }
+  
+    updateEmail(newEmail: string): void {
+      this.email = newEmail;
+      this.updatedAt = new Date();
+    }
+  
+    updatePassword(newPassword: string): void {
+      this.password = newPassword;
+      this.updatedAt = new Date();
+    }
+  
+    getProfile(): { id: number; name: string; email: string } {
+      return {
+        id: this.id,
+        name: this.name,
+        email: this.email,
+      };
+    }
+  }
+  
